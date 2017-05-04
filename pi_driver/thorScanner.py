2ca11e9fe4d3306dc39f5130dd706853ae1fb844
shaon01 <shaoun46@gmail.com>
2017-05-04 22:59:53 +0000

    Added a scanning scrip which moves the camera and shows image

---
+++ pi_driver/thorScanner.py
@@ -0,0 +1,67 @@
+#import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time,struct,serial
+import cv2
+
+serialCom = serial.Serial("/dev/ttyAMA0",9600,timeout=1)
+serialCom.isOpen()
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+cameraAngle = 0 
+reverseDir = 0 
+
+def moveCamera():
+	#moving the camera from right to left
+	global reverseDir
+	global cameraAngle
+	print 'reading global :',reverseDir
+
+	if (reverseDir == 0):
+		cameraAngle = cameraAngle + 5
+		if (cameraAngle >= 160):
+			reverseDir = 1
+
+	elif (reverseDir == 1 ):
+		cameraAngle = cameraAngle - 5
+		if (cameraAngle <= 0 ):
+			reverseDir = 0
+
+	serialCom.write(struct.pack('!cc','b',chr(cameraAngle)))
+	print 'camera angle : ',cameraAngle
+	time.sleep(0.1)
+	
+ 
+# allow the camera to warmup
+
+time.sleep(0.1)
+
+
+if __name__ == "__main__":
+#make the camera facing up
+	serialCom.write(struct.pack('!cc','h',chr(30)))
+ 
+# capture frames from the camera
+	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image
+	
+		image = frame.array
+ 
+	# show the frame
+		cv2.imshow("Frame", image)
+		key = cv2.waitKey(1) & 0xFF
+ 	
+	#moving the camera
+		moveCamera()
+	# clear the stream in preparation for the next frame
+		rawCapture.truncate(0)
+ 
+	# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+
